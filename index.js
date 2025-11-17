16ba6be6aa6c4e358419f8ea361bfc45318cca9a
duyp6090 <duyp6090@gmail.com>
2025-11-17 09:29:26 +0700

    update js

---
+++ index.js
@@ -0,0 +1,118 @@
+require([
+  "esri/Map",
+  "esri/views/SceneView",
+  "esri/layers/GraphicsLayer",
+  "esri/geometry/Mesh",
+  "esri/geometry/Point",
+  "esri/Graphic",
+  "esri/widgets/Slice",
+  "esri/analysis/SlicePlane",
+  "esri/widgets/LayerList",
+  "esri/core/Collection",
+  "esri/core/reactiveUtils",
+], function (
+  Map,
+  SceneView,
+  GraphicsLayer,
+  Mesh,
+  Point,
+  Graphic,
+  Slice,
+  SlicePlane,
+  LayerList,
+  Collection,
+  reactiveUtils
+) {
+  const sketchLayer = new GraphicsLayer({
+    elevationInfo: {
+      mode: "absolute-height",
+    },
+    title: "Sketched geometries",
+  });
+  const map = new Map({
+    basemap: "topo-vector",
+    ground: "world-elevation",
+  });
+
+  const view = new SceneView({
+    container: "viewDiv",
+    map: map,
+    camera: {
+      position: [106.8008663430442, 10.863971396764873, 300],
+      heading: 0,
+      tilt: 50,
+    },
+  });
+
+  const plane = new SlicePlane({
+    position: {
+      latitude: 10.867532728640802,
+      longitude: 106.80084488537197,
+      z: 50,
+    },
+    tilt: 32.62,
+    width: 100,
+    height: 100,
+    heading: 0.46,
+  });
+  let sliceWidget = null;
+  let doorsLayer = null;
+  let sliceTiltEnabled = true;
+  const excludedLayers = [];
+
+  sketchLayer.when(() => {
+    // Iterate through the flat array of sublayers and extract the ones
+    // that should be excluded from the slice widget
+
+    setSliceWidget();
+  });
+
+  function setSliceWidget() {
+    sliceWidget = new Slice({
+      view: view,
+      container: "sliceContainer",
+    });
+    sliceWidget.viewModel.excludedLayers.addMany(excludedLayers);
+    sliceTiltEnabled = true;
+    sliceWidget.viewModel.tiltEnabled = sliceTiltEnabled;
+    sliceWidget.viewModel.shape = plane;
+    reactiveUtils.watch(
+      () => sliceWidget.viewModel.state,
+      (state) => {}
+    );
+  }
+
+  const house = new Point({
+    x: 106.80084488537197,
+    y: 10.867532728640802,
+    z: 20.2,
+  });
+
+  Mesh.createFromGLTF(house, "./GisLab2_roomglb.glb")
+    .then(function (geometry) {
+      geometry.scale(50, { origin: house });
+      geometry.rotate(0, 0, 300);
+      const graphic = new Graphic({
+        geometry,
+        symbol: {
+          type: "mesh-3d",
+          symbolLayers: [
+            {
+              type: "fill",
+              size: 10000,
+            },
+          ],
+        },
+      });
+      sketchLayer.add(graphic);
+    })
+    .catch(console.error);
+
+  const car = new Point({
+    x: 106.80080197002785,
+    y: 10.86677410529397,
+    z: 20.2,
+  });
+
+  map.add(sketchLayer);
+});
